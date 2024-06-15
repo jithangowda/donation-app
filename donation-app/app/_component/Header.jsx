b45b8a8a5faf003a832756d7a311326716877db2
jithangowda <jithangck714@gmail.com>
2024-06-15 20:15:38 +0530

    added logo

--- donation-app/app/_component/Header.jsx
+++ donation-app/app/_component/Header.jsx
@@ -1,8 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+        <Image src={'/logo.svg'} width={150} height={150} alt='logo'/>
+    </div>
   )
 }
 
