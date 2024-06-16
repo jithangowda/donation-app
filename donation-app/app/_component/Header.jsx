8502b3e9be9d26511d2888ec39c47b63fdabb13e
jithangowda <jithangck714@gmail.com>
2024-06-16 19:42:13 +0530

    v2

--- donation-app/app/_component/Header.jsx
+++ donation-app/app/_component/Header.jsx
@@ -1,5 +1,6 @@
 "use client"
 import { Button } from '@/components/ui/button'
+import { UserButton, useUser } from '@clerk/nextjs'
 import { Plus } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -9,13 +10,15 @@ import React, { useEffect } from 'react'
 function Header() {
   const pathname = usePathname();
 
+  const {user, isSignedIn} = useUser();
+
   useEffect(()=>{
     console.log(pathname)
   },[])
 
   return (
     <div className='p-4 px-5 flex justify-between shadow-sm fixed top-0 w-full z-50 bg-white'>
-      <div className='flex gap-7 item-center'>
+      <div className='flex gap-10 item-center'>
         <Image src={'/logo.svg'} width={150} height={150} alt='logo'/>
         <ul className='hidden md:flex gap-10 py-4'>
           <Link href={'/'}>
@@ -28,9 +31,11 @@ function Header() {
           <li className='hover:text-primary font-medium text-sm cursor-pointer'>Link 2</li>
         </ul>
       </div>
-      <div className='flex gap-2 py-1'>
+      <div className='flex gap-2 py-1 items-center'>
         <Button className='flex gap-2'><Plus/>Post your listing</Button>
-        <Button variant='outline'>Login</Button>
+
+        {isSignedIn ? <UserButton/> : <Button variant='outline'>Login</Button>}
+        
       </div>
     </div>
   )
